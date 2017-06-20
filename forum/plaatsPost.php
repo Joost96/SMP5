d6cc9435a7b356a637c687ed8843387064d54189
Joost96 <joost-k@live.nl>
2017-06-20 16:34:10 +0200

    Yannick forum 20-06

---
+++ forum/plaatsPost.php
@@ -0,0 +1,17 @@
+<?php
+	include_once (dirname(__DIR__)."/model/forumDAO.php");
+	include_once (dirname(__DIR__)."/model/ForumPostModel.php");
+	
+	$forumdao = new forumDAO();
+	
+	echo "3";
+	
+	$post = new ForumPostModel(null, $_POST['onderwerp_id'], $_POST['naam'], $_POST['content']);
+	
+	var_dump($post);
+	
+	$forumdao->plaatsPost($post);
+	
+	header("location: onderwerp.php?onderwerp_id={$_POST['onderwerp_id']}");
+	
+?>
