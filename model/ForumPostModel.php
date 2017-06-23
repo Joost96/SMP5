a76ba187cbf52a9d92caa19a135e0b18b02b6e90
joshua <joshuadaveyvolkers@hotmail.com>
2017-06-23 14:53:13 +0200

    ForumPostModel aangepast

--- model/ForumPostModel.php
+++ model/ForumPostModel.php
@@ -4,12 +4,16 @@
 		public $onderwerpId;
 		public $titel;
 		public $content;
+		public $auteurId;
+		public $datum;
 		
-		public function __construct($id, $onderwerpId, $titel, $content){
+		public function __construct($id, $onderwerpId, $titel, $content, $auteurId, $datum){
 			$this->id = $id;
 			$this->onderwerpId = $onderwerpId;
 			$this->titel = $titel;
 			$this->content = $content;
+			$this->auteurId = $auteurId;
+			$this->datum = $datum;
 		}
 	}
 ?>
